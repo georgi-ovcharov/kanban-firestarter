01a60dcf48fcf817dba84517de0669ba16a2ec30
georgi-ovcharov <georgi.ovcharov12@gmail.com>
2022-08-16 13:03:22 +0300

    created email-login, guard, snack service, kanban modules

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,9 +6,10 @@ import { GoogleSigninDirective } from './google-signin.directive';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 @NgModule({
-  declarations: [GoogleSigninDirective, LoginPageComponent],
+  declarations: [GoogleSigninDirective, LoginPageComponent, EmailLoginComponent],
   exports: [GoogleSigninDirective],
   imports: [CommonModule, SharedModule, UserRoutingModule, ReactiveFormsModule],
 })
